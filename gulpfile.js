0c365511bf50b17abd7180bbda7632cbc7dbfff9
Enzo <enzocv96@gmail.com>
2016-07-18 12:46:34 -0500

    firts commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+
+//tareas
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({			browsers: ['last 2 versions'],			cascade: true		}))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./scss/**/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("./js/*.js").on('change', browserSync.reload);
+});
