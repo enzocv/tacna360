a684068105dbdea744314689a23b56a63c850651
Enzo <enzocv96@gmail.com>
2016-07-21 17:31:33 -0500

    changes added scripts.js(function click |remove class mostrar)

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
 var toggleMenu = document.getElementById('toggleMenu'),
 menu = document.getElementById('menu'),
-body = document.getElementById('body');
+body = document.getElementById('body'),
+link = document.getElementsByClassName('main-manu__link');
 
-toggleMenu.addEventListener('click',function(){
-    menu.classList.toggle('mostrar');
-    body.classList.toggle('mostrar');
-});
+/**
+ * [addEventListener description]
+ * @param {[id]} 'click'   [toggleMenu para que el menu se muestre]
+ * @param {[type]} function( [añade la clase MOSTRAR a el body y al menu]
+ */
+    toggleMenu.addEventListener('click',function(){
+        menu.classList.toggle('mostrar');
+        body.classList.toggle('mostrar');
+
+    });
+
+/**
+ * [addEventListener description]
+ * @param {[class]} 'click'   [description]
+ * @param {[type]} function( [remueve la clase mostrar cuando se de clic
+ *  en  un main-menu__link]
+ */
+        var i = link.length - 1;
+        for (; i >= 0 ; i--) {
+             link[i].addEventListener('click',function(){
+                 menu.classList.remove('mostrar');
+                 body.classList.remove('mostrar');
+                 console.log("click");
+             });
+         }
